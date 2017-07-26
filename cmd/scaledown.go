03ceb9bb12a0880707bf1da59643f8181e931ae0
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-07-26 16:48:16 -0300

    scaleup and scaledown commands

---
+++ cmd/scaledown.go
@@ -0,0 +1,72 @@
+// Copyright © 2017 TopFreeGames backend@tfgco.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// scaledownCmd represents the scaledown command
+var scaledownCmd = &cobra.Command{
+	Use:   "scaledown",
+	Short: "Scales down rooms at a specific scheduler",
+	Long:  `Scales down rooms at a specific scheduler`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Error: inform scheduler name")
+			os.Exit(1)
+		}
+
+		schedulerName := args[0]
+
+		if amount == 0 {
+			fmt.Println("Error: inform amount > 0")
+			os.Exit(1)
+		}
+
+		log := newLog("scaledown")
+
+		config, err := getConfig()
+		if err != nil {
+			log.WithError(err).Fatal("error getting client config")
+		}
+		client := getClient(config)
+
+		log.Debugf("reading %s", schedulerName)
+
+		fmt.Printf("Scaling down scheduler '%s' in %d rooms, this may take a few minutes...\n", schedulerName, amount)
+
+		url := fmt.Sprintf("%s/scheduler/%s?scaledown=%d", config.ServerURL, schedulerName, amount)
+		body, status, err := client.Post(url, "")
+		if err != nil {
+			log.WithError(err).Fatal("error on delete request")
+		}
+		if status != http.StatusOK {
+			printError(body)
+			return
+		}
+
+		fmt.Println("Successfully scaled down scheduler", schedulerName)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(scaledownCmd)
+	scaledownCmd.Flags().UintVarP(&amount, "amount", "a", 0, "Amount of rooms to scale down")
+}
---
+++ cmd/scaleup.go
@@ -0,0 +1,74 @@
+// Copyright © 2017 TopFreeGames backend@tfgco.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var amount uint
+
+// scaleupCmd represents the scaleup command
+var scaleupCmd = &cobra.Command{
+	Use:   "scaleup",
+	Short: "Scales up rooms at a specific scheduler",
+	Long:  `Scales up rooms at a specific scheduler`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Error: inform scheduler name")
+			os.Exit(1)
+		}
+
+		schedulerName := args[0]
+
+		if amount == 0 {
+			fmt.Println("Error: inform amount > 0")
+			os.Exit(1)
+		}
+
+		log := newLog("scaleup")
+
+		config, err := getConfig()
+		if err != nil {
+			log.WithError(err).Fatal("error getting client config")
+		}
+		client := getClient(config)
+
+		log.Debugf("reading %s", schedulerName)
+
+		fmt.Printf("Scaling up scheduler '%s' in %d rooms, this may take a few minutes...\n", schedulerName, amount)
+
+		url := fmt.Sprintf("%s/scheduler/%s?scaleup=%d", config.ServerURL, schedulerName, amount)
+		body, status, err := client.Post(url, "")
+		if err != nil {
+			log.WithError(err).Fatal("error on delete request")
+		}
+		if status != http.StatusOK {
+			printError(body)
+			return
+		}
+
+		fmt.Println("Successfully scaled up scheduler", schedulerName)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(scaleupCmd)
+	scaleupCmd.Flags().UintVarP(&amount, "amount", "a", 0, "Amount of rooms to scale up")
+}
